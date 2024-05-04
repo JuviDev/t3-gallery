d25927657dc4f6e28e4d32435ddd2ae0c4c43873
juvidev <jvivas@correo.iue.edu.co>
2024-05-03 20:30:57 -0500

    Add scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+const mockUrls = [
+  "https://utfs.io/f/623b6961-85b9-41ff-a073-d740c55a3d93-1psigh.jpg",
+  "https://utfs.io/f/d8d986df-16fd-4c90-ae24-a1a834588487-komxmc.png",
+  "https://utfs.io/f/bb9a74f1-5fec-4bbc-9705-f7bc567ab1ab-snoqzm.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hi there! Gallery coming soon!
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <picture>
+              <source type="image/webp" srcSet={`${image.url}?format=webp`} />
+              <img
+                src={`${image.url}?format=jpeg`}
+                alt="mock image"
+                className="h-48 w-full object-cover"
+              />
+            </picture>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
