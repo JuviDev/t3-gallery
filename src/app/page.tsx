9b624fac5062e10685f20dceb118df68ba24d389
juvidev <jvivas@correo.iue.edu.co>
2024-05-16 11:10:07 -0500

    Use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,46 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <picture>
-              <source type="image/webp" srcSet={`${image.url}?format=webp`} />
-              <img
-                src={`${image.url}?format=jpeg`}
-                alt="mock image"
-                className="h-48 w-full object-cover"
-              />
-            </picture>
-            <div>
-              <p>{image.name}</p>
-            </div>
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <picture>
+            <source type="image/webp" srcSet={`${image.url}?format=webp`} />
+            <img
+              src={`${image.url}?format=jpeg`}
+              alt="mock image"
+              className="h-48 w-full object-cover"
+            />
+          </picture>
+          <div>
+            <p>{image.name}</p>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="text-2x1 h-full w-full text-center">
+          Please sign in above.
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
