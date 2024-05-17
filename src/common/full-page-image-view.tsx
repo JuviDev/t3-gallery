6f3229036be4f8a6a83cff51db4adba608c78613
juvidev <jvivas@correo.iue.edu.co>
2024-05-17 16:09:09 -0500

    Finalize modal and parallel iamge routes

--- src/common/full-page-image-view.tsx
+++ src/common/full-page-image-view.tsx
@@ -1,3 +1,4 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { getImage } from "~/server/queries";
 
@@ -6,6 +7,7 @@ export async function FullPageImageView(props: { id: string }) {
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
   const image = await getImage(idAsNumber);
+  const userInfo = await clerkClient.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-full min-w-0 ">
@@ -18,8 +20,18 @@ export async function FullPageImageView(props: { id: string }) {
           alt={image.name}
         />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-48 flex-shrink-0 flex-col gap-2 border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+
+        <div className="flex flex-col px-2">
+          <span>Uploaded By:</span>
+          <span>{userInfo.fullName}</span>
+        </div>
+
+        <div className="flex flex-col px-2">
+          <span>Created On:</span>
+          <span>{image.createdAt.toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
